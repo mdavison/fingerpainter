7d61f4d2027a3f5a156b3f927e1f0c2f87e4f26a
Morgan Davison <morgan.davison@gmail.com>
2015-11-23 06:38:59 -0800

    Initial Commit

---
+++ Fingerpainter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fingerpainter
+//
+//  Created by Morgan Davison on 11/23/15.
+//  Copyright © 2015 Morgan Davison. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

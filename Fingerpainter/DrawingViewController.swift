6dd45671c2787ce2f865b8f4a56c111abd6195dc
Morgan Davison <morgan.davison@gmail.com>
2016-01-11 09:20:19 -0800

    Add confirm alert to delete button

--- Fingerpainter/DrawingViewController.swift
+++ Fingerpainter/DrawingViewController.swift
@@ -181,7 +181,18 @@ class DrawingViewController: UIViewController, UIPopoverPresentationControllerDe
     // MARK: - Actions
     
     @IBAction func clearImage(sender: UIBarButtonItem) {
-        canvas.image = nil 
+        let alert = UIAlertController(title: "Clear Canvas", message: "Are you sure you want to clear the canvas?", preferredStyle: .Alert)
+        let clearAction = UIAlertAction(title: "Clear", style: .Destructive) { (alert: UIAlertAction!) -> Void in
+            self.canvas.image = nil
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .Default) { (alert: UIAlertAction!) -> Void in
+            //print("You pressed Cancel")
+        }
+        
+        alert.addAction(clearAction)
+        alert.addAction(cancelAction)
+        
+        presentViewController(alert, animated: true, completion:nil)
     }
     
     @IBAction func colorChanged(sender: AnyObject) {
